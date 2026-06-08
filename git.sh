6531227df149bbec0eb0dfa0ae68f59fa851b052
Eric Thomas Schneider <ericts@vt.edu>
2026-06-08 16:50:27 +0000

    keep me tf away from nano

--- git.sh
+++ git.sh
@@ -4,3 +4,4 @@ git config --global user.name "Eric Thomas Schneider"
 git config --global init.defaultBranch main
 git config --global fetch.prune true
 git config --global merge.conflictstyle diff3
+git config --global core.editor vim
